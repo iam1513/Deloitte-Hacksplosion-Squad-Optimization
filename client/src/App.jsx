7221d7252d3ae97ac24c7842d23d0d7e4f573534
iam1513 <126111227+iam1513@users.noreply.github.com>
2025-02-01 09:39:15 +0530

    FEAT : Frontend initialized. Added a Pre-Loader animation.

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import PreLoader from './Components/PreLoader'
+
+function App() {
+
+  return (
+    <>
+      <div >
+        <PreLoader />
+      </div>
+    </>
+  )
+}
+
+export default App
